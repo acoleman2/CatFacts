205b703c989810b2eeba3733b2abd4adadc21641
Ariya C <acoleman@bsd220.org>
2024-03-14 09:26:33 -0500

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Ariya Coleman on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
